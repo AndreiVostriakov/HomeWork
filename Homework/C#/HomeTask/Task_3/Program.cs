5316d7d5c1624b1fe1d98be1191b24312dafdb7d
Andrei_Vostriakov <cursed25@yandex.ru>
2023-01-28 23:38:10 +0500

    Решение задачи №6. Первый семинар

---
+++ Homework/C#/HomeTask/Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на входе принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    Console.WriteLine("Число " + num + " является чётным.");
+}
+else
+{
+    Console.WriteLine("Число " + num + " не чётное.");
+}
