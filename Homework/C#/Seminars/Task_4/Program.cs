bcca27efbc8ab758f2772b72146f73786e6059ff
Andrei_Vostriakov <cursed25@yandex.ru>
2023-01-28 22:26:56 +0500

    Создали 4 проект

---
+++ Homework/C#/Seminars/Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, котора на входе принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int K = -N;
+
+while(K <= N)
+{
+    Console.Write(K + " ");
+    K++;
+}
