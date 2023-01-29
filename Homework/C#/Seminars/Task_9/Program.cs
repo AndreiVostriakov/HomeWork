2688fabed02aef809b4c89dd6aca39e8edc5635c
Andrei_Vostriakov <cursed25@yandex.ru>
2023-01-29 19:05:19 +0500

    Снова отредактировал проект

--- Homework/C#/Seminars/Task_9/Program.cs
+++ Homework/C#/Seminars/Task_9/Program.cs
@@ -9,8 +9,8 @@ int num = int.Parse(Console.ReadLine()!);
 if (num % 7 == 0 && num % 23 == 0)
 {
     Console.WriteLine($"{num} кратно 7 и 23");
-} 
+}
 else
 {
     Console.WriteLine($"{num} не кратно 7 и 23");
-} 
+}
