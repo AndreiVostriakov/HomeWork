ca5dc421cb90bc22cfbd000eef0ff501cffcd8e8
Andrei_Vostriakov <cursed25@yandex.ru>
2023-01-29 18:51:00 +0500

    решение домашней задачи №15. Второй семинар

---
+++ Homework/C#/HomeTask/Task_7/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+
+string[] week = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+
+Console.Write("Введите цифру обозначающую день недели: ");
+int number = int.Parse(Console.ReadLine());
+
+int index = number - 1;
+
+if (index < week.Length)
+{
+    if (number <= 5)
+    {
+        Console.WriteLine($"Дак, это {week[index]} и это рабочий день");
+    }
+    else
+    {
+        Console.WriteLine($"Дак, это {week[index]} и это выходной день");
+    }
+}
+else
+{
+    Console.WriteLine("Такого дня недели нет");
+}
+
