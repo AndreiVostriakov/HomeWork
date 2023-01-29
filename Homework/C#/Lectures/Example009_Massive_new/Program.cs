d86fc35c58c31a9a18faa27696d754a29c9f858b
Andrei_Vostriakov <cursed25@yandex.ru>
2023-01-29 11:43:51 +0500

    Создали новый проект с решением массива

---
+++ Homework/C#/Lectures/Example009_Massive_new/Program.cs
@@ -0,0 +1,56 @@
+﻿//  Найти максимальное значение из 9 чисел
+
+Console.Clear();
+
+void FillArray(int[] collectoin)
+{
+    int length = collectoin.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collectoin[index] = new Random().Next(1, 10);
+        index++;
+    }
+
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    
+    while(index < count)
+    {
+        if(collection[index] == find);
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[4] = 4;
+
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
