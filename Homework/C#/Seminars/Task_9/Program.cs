35841f7f2e6a6cdfa61416348cd84d5451855d4d
Andrei_Vostriakov <cursed25@yandex.ru>
2023-01-29 13:49:31 +0500

    Создали проект задачи №9. Второй семинар

---
+++ Homework/C#/Seminars/Task_9/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая будет принимать на вход два число и проверяет, кратно ли оно одновременно 7 и 23
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+
+if (num1 % 7 == 0 && num % 23 == 0) Console.WriteLine($"{num} кратно 7 и 23");
+else Console.WriteLine($"{num} не кратно 7 и 23");
+
+
