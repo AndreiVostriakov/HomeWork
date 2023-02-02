5b3827195a305350ae666ea8ad910b67b0213994
Andrei_Vostriakov <cursed25@yandex.ru>
2023-02-02 12:46:48 +0500

    Добавил ещё один вариант решения задачи

--- Homework/C#/HomeTask/Task_7/Program.cs
+++ Homework/C#/HomeTask/Task_7/Program.cs
@@ -1,28 +1,47 @@
-﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+﻿// // Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// Console.Clear();
+
+// string[] week = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+
+// Console.Write("Введите цифру обозначающую день недели: ");
+// int number = int.Parse(Console.ReadLine());
+
+// int index = number - 1;
+
+// if (index < week.Length)
+// {
+//     if (number <= 5)
+//     {
+//         Console.WriteLine($"Дак, это {week[index]} и это рабочий день");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Дак, это {week[index]} и это выходной день");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Такого дня недели нет");
+// }
+
+
 
 Console.Clear();
-
-string[] week = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
-
 
 Console.Write("Введите цифру обозначающую день недели: ");
 int number = int.Parse(Console.ReadLine());
 
-int index = number - 1;
-
-if (index < week.Length)
+if (number > 0 && number <= 5)
 {
-    if (number <= 5)
-    {
-        Console.WriteLine($"Дак, это {week[index]} и это рабочий день");
-    }
-    else
-    {
-        Console.WriteLine($"Дак, это {week[index]} и это выходной день");
-    }
+    Console.WriteLine("Это рабочий день");
+}
+else if (number >5 && number <= 7)
+{
+    Console.WriteLine("Это выходной день");
 }
 else
 {
-    Console.WriteLine("Такого дня недели нет");
+    Console.WriteLine("Такого дня недели не существует");
 }
-
