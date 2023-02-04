bccfc72be5ab65ab7d4f6b4c41fddbe3824677f8
Andrei_Vostriakov <cursed25@yandex.ru>
2023-02-04 12:43:44 +0500

    Решение задачи. Третий семинар

---
+++ Homework/C#/Seminars/Task_11/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу которая принимает на вход координаты точки (Х и Y), и выдаёт номер четверти в плоскости в которой находится точка.
+
+Console.Clear();
+
+Console.Write("Введите число X: ");
+int X = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число Y: ");
+int Y = int.Parse(Console.ReadLine());
+
+if (Y > 0)
+{
+    if (X > 0)
+    {
+        Console.Write("Первая четверть");
+    }
+    else
+    {
+        Console.Write("Вторая четверть");
+    }
+}
+else
+{
+    if (X < 0)
+    {
+        Console.Write("Третья четверть");
+    }
+    else
+    {
+        Console.Write("Четвёртая четверть");
+    }
+}
