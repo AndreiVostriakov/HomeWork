bd1873afe313d2818a47e98716746139688b31d5
Andrei_Vostriakov <cursed25@yandex.ru>
2023-02-05 13:44:24 +0500

    Решение задачию. Семинар 4

---
+++ Homework/C#/Seminars/Task_17/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+// Определить количество цифр в числе
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+
+int count = 0;
+
+while (A != 0)
+{
+    A = A / 10;
+    count++;
+}
+
+Console.Write(count);
+
+
+// int count = 1;
+
+// while (A >= 0 || A <= -10)
+// {
+//     A = A / 10;
+//     count++;
+// }
+
+// Console.Write(count);
