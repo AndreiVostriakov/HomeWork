a2be411e972e9d340b680c886f1e9e0a14b18da6
Andrei_Vostriakov <cursed25@yandex.ru>
2023-02-05 13:44:08 +0500

    Решение задачию. Семинар 4

---
+++ Homework/C#/Seminars/Task_15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Посчитать сумму чисел от 1 до А
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int count = 0;
+for (int i = 1; i <= num; i++)
+{
+    count += i;
+}
+Console.Write(count);
+
+
+
+
+
+
