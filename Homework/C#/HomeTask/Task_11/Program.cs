985662054792bd8d9fd84e4f6eee2c247626941d
Andrei_Vostriakov <cursed25@yandex.ru>
2023-02-05 20:00:12 +0500

    Решение домашней задачи. Четвёртый семинар

---
+++ Homework/C#/HomeTask/Task_11/Program.cs
@@ -0,0 +1,16 @@
+﻿// Посчитать сумму цифр в числе
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int result = num % 10;
+
+while (num != 0)
+{
+    num /= 10;
+    result += num % 10;
+}
+
+Console.WriteLine($"Сумма цифр в числе равна {result}");
