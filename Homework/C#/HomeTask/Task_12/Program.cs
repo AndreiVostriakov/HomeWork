078070358c88688b904a0f18a2b14b2e79d18606
Andrei_Vostriakov <cursed25@yandex.ru>
2023-02-05 20:15:50 +0500

    Решение домашней задачи. Четвёртый семинар

---
+++ Homework/C#/HomeTask/Task_12/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int result = 1;
+
+for (int i = 1; i <= N; i++)
+{
+    result *= i;
+}
+Console.WriteLine($"Произведение чисел от 1 до {N} равно {result}");
