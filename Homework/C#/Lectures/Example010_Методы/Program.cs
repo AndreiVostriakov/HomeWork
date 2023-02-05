330e7cfd56d3421321c6b102aee70972676ef9ed
Andrei_Vostriakov <cursed25@yandex.ru>
2023-02-05 11:15:14 +0500

    Закоментировал примеры

--- Homework/C#/Lectures/Example010_Методы/Program.cs
+++ Homework/C#/Lectures/Example010_Методы/Program.cs
@@ -1,61 +1,61 @@
-﻿// Вид 1
-void Method1()
-{
-    Console.WriteLine("Автор ...");
-}
+﻿// // Вид 1
+// void Method1()
+// {
+//     Console.WriteLine("Автор ...");
+// }
 
-Method1();
-
-
-// Вид 2
-void Method2(string msg)
-{
-    Console.WriteLine(msg);
-}
-Method2("Текст сообщения");
+// Method1();
 
 
-
-void Method21(string msg, int count)
-{
-    int i = 0;
-    while (i < count)
-    {
-        Console.WriteLine(msg);
-        i++;
-    }
-}
-Method21(msg: "Текст", count: 4);
+// // Вид 2
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2("Текст сообщения");
 
 
 
-// Вид 3
-int Method3()
-{
-    return DataTime.Now.Year;
-}
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// Method21(msg: "Текст", count: 4);
 
-int year = Method3();
-Console.WriteLine(year);
+
+
+// // Вид 3
+// int Method3()
+// {
+//     return DataTime.Now.Year;
+// }
+
+// int year = Method3();
+// Console.WriteLine(year);
 
 
 
-// Вид 4
+// // Вид 4
 
-int Method4(int count, string text)
-{
-    int i = 0;
-    string result = String.Empty;
-    while (i < count)
-    {
-        result = result + text;
-        i++;
-    }
-    return result;
-}
+// int Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
 
-string res = Method4(10, "asdf");
-Console.WriteLine(res);
+// string res = Method4(10, "asdf");
+// Console.WriteLine(res);
 
 
 
