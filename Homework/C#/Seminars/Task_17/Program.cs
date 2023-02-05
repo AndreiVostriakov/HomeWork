12899d19bc3e2141b3f1101176679ada2f88b442
Andrei_Vostriakov <cursed25@yandex.ru>
2023-02-05 13:46:47 +0500

    Добавил ещё одно решение. Семинар 4

--- Homework/C#/Seminars/Task_17/Program.cs
+++ Homework/C#/Seminars/Task_17/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.InteropServices;
-// Определить количество цифр в числе
+﻿// Определить количество цифр в числе
 
 Console.Clear();
 
@@ -24,5 +23,32 @@ Console.Write(count);
 //     A = A / 10;
 //     count++;
 // }
-
 // Console.Write(count);
+
+
+
+
+// // Определить количество цифр в числе.
+
+// int CountOfNum(int num)
+// {
+//     int count = 0;
+//     if (num == 0) count = 1;
+//     else
+//     {
+//         while (num != 0)
+//         {
+//             num /= 10;
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// Console.Clear();
+
+// Console.Write("Введите число: ");
+// int num = int.Parse(Console.ReadLine()!);
+// int res = CountOfNum(num);
+
+// Console.WriteLine($"В числе {res} цифр");
