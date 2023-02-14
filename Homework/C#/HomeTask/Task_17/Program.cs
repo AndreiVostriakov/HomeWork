fee40b186ce7c8c87d5814046c249df16fa45689
Andrei_Vostriakov <cursed25@yandex.ru>
2023-02-14 22:01:31 +0500

    Решение домашней задачи со звёздочкой. Пятый семинар

---
+++ Homework/C#/HomeTask/Task_17/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задайте массив, заполненный случайными числами.
+//разработать алгоритм сортировки методом пузырька.Реализовать возврастающую сортировку.
+
+int[] Getarray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+int[] sortArray(int[] OldArray)
+{
+    int temp;
+    for (int i = 0; i < OldArray.Length - 1; i++)
+    {
+        for (int j = i + 1; j < OldArray.Length; j++)
+        {
+            if (OldArray[i] > OldArray[j])
+            {
+                temp = OldArray[i];
+                OldArray[i] = OldArray[j];
+                OldArray[j] = temp;
+            }
+        }
+    }
+    return OldArray;
+}
+
+Console.Clear();
+
+int[] array = Getarray(10, 0, 10);
+Console.WriteLine(string.Join(", ", array));
+
+int[] newArray = sortArray(OldArray: array);
+Console.WriteLine(newArray);
