4191b8f9bf267a15a8ef07c5bb2dc566d369c6a1
Andrei_Vostriakov <cursed25@yandex.ru>
2023-02-17 22:38:31 +0500

    Решение задачи. Шестой семинар

---
+++ Homework/C#/Seminars/Task_23/Program.cs
@@ -0,0 +1,44 @@
+﻿// напишите программу которая перевернёт одномерный массив.
+// (последний элемент будет на первом месте, а первый на последнем)
+// [1, 2, 3, 4, 5, 6] == [6, 5, 4, 3, 2, 1]
+// [6, 7, 3, 6] == [6, 3, 7, 6]
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+void ReversArray1(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length / 2; i++)
+    {
+        int temp = inArray[i];
+        inArray[i] = inArray[inArray.Length - 1 - i];
+        inArray[inArray.Length - 1 - i] = temp;
+
+    }
+}
+
+int[] ReversArray2(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        result[i] = inArray[inArray.Length - 1 - i];
+    }
+    return result;
+}
+
+int[] array = GetArray(5, 0, 10);
+Console.WriteLine(string.Join(", ", array));
+ReversArray1(array);
+Console.WriteLine(string.Join(", ", array));
+int[] reversed = ReversArray2(array);
+Console.WriteLine(string.Join(", ", reversed));
