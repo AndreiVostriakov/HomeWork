96f4f2bdd18856831dbffbc4028f0d899e237cd4
Andrei_Vostriakov <cursed25@yandex.ru>
2023-02-18 00:08:01 +0500

    Решение задачи про копирование массива. Шестой семинар

---
+++ Homework/C#/Seminars/Task_27/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования
+
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+int[] CopyArray(int[] inArray)
+{
+    int[] array = new int[inArray.Length];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        array[i] = inArray[i];
+    }
+    return array;
+
+}
+
+int[] array = GetArray(5, -10, 10);
+Console.WriteLine(string.Join(", ", array));
+int[] CopyArr = CopyArray(array);
+Console.WriteLine(string.Join(", ", CopyArr));
