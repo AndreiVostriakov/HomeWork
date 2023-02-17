a6718102b6576e47269af567e8256ac2827a9d95
Andrei_Vostriakov <cursed25@yandex.ru>
2023-02-17 23:23:11 +0500

    Решение задачи на перевод числа в двичное число. Шестой семинар

---
+++ Homework/C#/Seminars/Task_25/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Напишите программу, которая будет преобразовывать 
+// десятичное число в двоичное
+
+Console.Clear();
+
+string DecToBin(int num)
+{
+    string result = "";
+    while (num > 0)
+    {
+        result = (num % 2).ToString() + result;
+        num /= 2;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine(DecToBin(x));
