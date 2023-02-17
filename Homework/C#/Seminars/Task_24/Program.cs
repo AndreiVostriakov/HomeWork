479bc1537b4216d2a3dee94789c5aa886067a954
Andrei_Vostriakov <cursed25@yandex.ru>
2023-02-17 23:02:00 +0500

    Решение задачи. Шестой семинар

---
+++ Homework/C#/Seminars/Task_24/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три 
+// числа и проверяет, может ли существовать треугольник со сторонами такой длины
+
+bool CheckTreeEnd(int a, int b, int c)
+{
+    return(a + b > c && a + c > b && b + c > a);
+}
+
+Console.WriteLine("Введите первое число");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int y = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int z = int.Parse(Console.ReadLine());
+
+if (CheckTreeEnd(x, y, y))
+{
+    Console.WriteLine("Не существует");
+}
+else
+{
+    Console.WriteLine("Cуществует");
+}
