1d4c8c88b42a703f8e3e0fef309e3e20e5a20e2b
Andrei_Vostriakov <cursed25@yandex.ru>
2023-02-17 23:51:42 +0500

    Решение задачи про фибоначи без рекурсии. Шестой семинар

---
+++ Homework/C#/Seminars/Task_26/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Не используя рекурсию, выведите первые N чисел 
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1
+
+Console.Clear();
+
+int[] Fibonacci(int N)
+{
+    int[] result = new int[N];
+    result[0] = 0;
+    result[1] = 1;
+    for (int i = 2; i < N; i++)
+    {
+        result[i] = result[i - 1] + result[i - 2];
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число");
+int x = int.Parse(Console.ReadLine());
+int[] array = Fibonacci(x);
+Console.WriteLine(string.Join(", ", array));
