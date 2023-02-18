5a686da0f3c61c5181b3e808d908aeab5d5160c1
Andrei_Vostriakov <cursed25@yandex.ru>
2023-02-18 21:02:56 +0500

    Решение домашней задачи №47. Седьмой семинар

--- Homework/C#/HomeTask/Task_20/Program.cs
+++ Homework/C#/HomeTask/Task_20/Program.cs
@@ -5,7 +5,7 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-double[,] GetArray(int m, int n, int minValue, int maxValue)
+double[,] GetArray(int m, int n)
 {
     double[,] result = new double[m, n];
 
@@ -13,7 +13,7 @@ double[,] GetArray(int m, int n, int minValue, int maxValue)
     {
         for (int j = 0; j < n; j++)
         {
-            result[i, j] = Math.Round(new Random().Next(minValue, maxValue + 1) + new Random().NextDouble(), 2);
+            result[i, j] = Math.Round(new Random().NextDouble(), 2);
         }
     }
     return result;
@@ -31,5 +31,9 @@ void PrintArray(double[,] inArray)
     }
 }
 
-double[,] array = GetArray(3, 4, 0, 10);
+Console.Write("Введите кол-во строк: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int col = int.Parse(Console.ReadLine());
+double[,] array = GetArray(row, col);
 PrintArray(array);
