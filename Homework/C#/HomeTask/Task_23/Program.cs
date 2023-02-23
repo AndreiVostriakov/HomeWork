1a034b57cd06ee7976de951d12340321fca64672
Andrei_Vostriakov <cursed25@yandex.ru>
2023-02-23 18:38:50 +0500

    Решение домашней задачи №54. Восьмой семинар

---
+++ Homework/C#/HomeTask/Task_23/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] sortArray(int[,] inArray)
+{
+    int temp;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int m = 1; m < inArray.GetLength(0); m++)
+            {
+                if (inArray[i, m] > inArray[i, m - 1])
+                {
+                    temp = inArray[i, m];
+                    inArray[i, m] = inArray[i, m - 1];
+                    inArray[i, m - 1] = temp;
+
+
+                }
+            }
+            ;
+        }
+    }
+    return inArray;
+}
+
+Console.Write("Введите кол-во строк: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбцов: ");
+int col = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(row, col, 0, 10);
+
+PrintArray(array);
+Console.WriteLine();
+int[,] NewArray = sortArray(array);
+PrintArray(NewArray);
