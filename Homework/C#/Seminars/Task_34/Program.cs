f6201e294fa97028c796f505a48041daf1bc9550
Andrei_Vostriakov <cursed25@yandex.ru>
2023-02-23 16:03:54 +0500

    Решение задачи №57. Восьмой семинар

--- Homework/C#/Seminars/Task_34/Program.cs
+++ Homework/C#/Seminars/Task_34/Program.cs
@@ -26,15 +26,16 @@ void PrintArray(int[,] inArray)
     }
 }
 
-int Conver2DTo1Darray(int[,] inArray)
+int[] Conver2DTo1Darray(int[,] inArray)
 {
-    int[] NewArray = new int [inArray.GetLength(0) * inArray(1)];
+    int[] NewArray = new int[inArray.GetLength(0) * inArray.GetLength(1)];
     int k = 0;
-    for (int i = 0; i < inArray.GetLength(0); i += 2)
+    for (int i = 0; i < inArray.GetLength(0); i++)
     {
-        for (int j = 0; j < inArray.GetLength(1); j += 2)
+        for (int j = 0; j < inArray.GetLength(1); j++)
         {
-            NewArray[k++] = inArray[i, j];
+            NewArray[k] = inArray[i, j];
+            k++;
         }
 
     }
@@ -61,16 +62,16 @@ void sortArray(int[] inArray)
 void PrintResult(int[] inArray)
 {
     int count = 1;
-    for (int i = 1; i < inArray.Length - 1; i++)
+    for (int i = 1; i < inArray.Length; i++)
     {
-        if (inArray[i] == inArray[1 - i])
+        if (inArray[i] == inArray[i - 1])
         {
             count++;
         }
         else
         {
             Console.WriteLine($" Количество {inArray[i - 1]} встречается {count} раз");
-            count = 0;
+            count = 1;
         }
     }
     Console.WriteLine($" Количество {inArray.Length - 1} встречается {count} раз");
@@ -88,90 +89,4 @@ Console.WriteLine();
 int[] Newarray = Conver2DTo1Darray(array);
 sortArray(Newarray);
 Console.WriteLine();
-PrintArray(Newarray);
-
-
-
-// /* Составить частотный словарь элементов двумерного массива.
-// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.*/
-// Console.Clear();
-// int[,] GetArray(int m, int n, int minValue, int maxValue)
-// {
-//     int[,] result = new int[m, n];
-
-//     for (int i = 0; i < m; i++)
-//     {
-//         for (int j = 0; j < n; j++)
-//         {
-//             result[i, j] = new Random().Next(minValue, maxValue + 1);
-//         }
-//     }
-//     return result;
-// }
-
-// void PrintArray(int[,] inArray)
-// {
-//     for (int i = 0; i < inArray.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < inArray.GetLength(1); j++)
-//         {
-//             Console.Write($"{inArray[i, j]}\t ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
-// int[] convert2DTo1D(int[,] inArray)
-// {
-//     int[] convert = new int[inArray.GetLength(0) * inArray.GetLength(1)];
-//     int k = 0;
-//     for (int i = 0; i < inArray.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < inArray.GetLength(1); j++)
-//         {
-//             convert[k++] = inArray[i, j];
-//         }
-//     }
-//     return convert;
-// }
-// void sortConvert(int[] inArray)
-// {
-//     int temp;
-//     for (int i = 0; i < inArray.Length - 1; i++)
-//     {
-//         for (int j = i + 1; j < inArray.Length; j++)
-//         {
-//             if (inArray[i] > inArray[j])
-//             {
-//                 temp = inArray[i];
-//                 inArray[i] = inArray[j];
-//                 inArray[j] = temp;
-//             }
-//         }
-//     }
-// }
-// void countResult(int[] inArray)
-// {
-//     int count = 1;
-//     for (int i = 1; i < inArray.Length; i++)
-//     {
-//         if (inArray[i] == inArray[i - 1])
-//         {
-//             count++;
-//         }
-//         else
-//         {
-//             Console.WriteLine($"Количество {inArray[i - 1]} встречается {count} раз");
-//             count = 1;
-//         }
-//     }
-//     Console.WriteLine($"Количество {inArray[inArray.Length-1]} встречается {count} раз");
-// }
-// Console.Write("Введите кол-во строк: ");
-// int row = int.Parse(Console.ReadLine()!);
-// Console.Write("Введите кол-во столбцов: ");
-// int col = int.Parse(Console.ReadLine()!);
-// int[,] array2D = GetArray(row, col, 0, 3);
-// PrintArray(array2D);
-// int[] convert = convert2DTo1D(array2D);
-// sortConvert(convert);
-// countResult(convert);
+PrintResult(Newarray);
