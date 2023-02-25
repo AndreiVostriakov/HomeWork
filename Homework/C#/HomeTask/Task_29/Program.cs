47aff431b08b9eff4a6f1fbef113c865769d36e6
Andrei_Vostriakov <cursed25@yandex.ru>
2023-02-25 23:37:28 +0500

    Решение домашней задачи №66. Девятый семинар

---
+++ Homework/C#/HomeTask/Task_29/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+
+int Recursiv(int N, int M)
+{
+    if (N == M) return N;
+    else
+    {
+        return Recursiv(N + 1, M) + N;
+    }
+
+}
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine());
+int sum = Recursiv(N, M);
+Console.Write(sum);
