5233cdbcc89d004956489d3b6aba1dc5e6fda7de
Luna <CurseD25@yandex.ru>
2023-04-06 18:02:34 +0500

    Create a file python

---
+++ Homework/Python/Introduction_to_language/lesson1/main.py
@@ -0,0 +1,21 @@
+# username = input("Введите имя пользователя: ")
+# name = "Маша"
+
+# if username == name:
+#     print("Ура это, " + name + "!")
+# else:
+#     print("Привет, " + username + "!")
+
+
+# num = 423
+# sum = 0
+# while num > 0:
+#     x = num % 10
+#     sum = sum + x
+#     num = num // 10
+# print(sum)
+
+
+# a = "qwerty"
+
+# print(a[::2])
