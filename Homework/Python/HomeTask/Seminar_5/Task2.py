b5207b236d9ac83d236fa2fa0b6748b333dc614e
Andrei_Vostriakov <cursed25@yandex.ru>
2023-04-28 23:51:02 +0500

    Решение домашней задачи №2. Пятый семинар

---
+++ Homework/Python/HomeTask/Seminar_5/Task2.py
@@ -0,0 +1,17 @@
+# Задача 2. Дан список случайных чисел. Создайте список, в который попадают числа, описывающие случайную возрастающую последовательность. Порядок элементов менять нельзя.
+# [1, 5, 2, 3, 4, 6, 1, 7] => [1, 2, 3] или [2, 7] или [4, 6, 7] и т.д.
+
+from random import randint
+
+nums = [randint(1, 9) for _ in range(8)]
+print(nums)
+
+def get_up2(nums):
+    ups = [nums[0]]
+    for i in nums:
+        if i > max(ups):
+            ups.append(i)
+    return ups
+    
+print(get_up2(nums))
+
