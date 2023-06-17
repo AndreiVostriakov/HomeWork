fb885139f5db06c23f20d5d46f43e981c137bb82
Luna <CurseD25@yandex.ru>
2023-06-18 01:50:57 +0500

    Решение задач на семинаре №1

---
+++ Homework/Python/Introduction_to_language_seminars/lesson1/Task.py
@@ -0,0 +1,29 @@
+# n = int(input("Какое кол-во км машина проезжает за 1 день: "))
+# m = int(input("Кол-во км проехала машина: "))
+# res = int(bool(m % n))
+# print("Кол-во дней = ", m//n + res)
+
+
+
+# amountClasses = 3
+# max = 2
+# firstClass = int(input('Enter A: '))
+# secondClass = int(input('Enter B: '))
+# thirdClass = int(input('Enter C: '))
+
+# desksAmount = ((firstClass // 2) + (firstClass % 2)) + ((secondClass // 2) + (secondClass % 2)) + ((thirdClass // 2) + (thirdClass % 2))
+# print(desksAmount)
+
+
+
+# d = int(input("Введите номер вагона: "))
+# e = int(input("Введите номер вагона: "))
+# print("Общая длина состава ", (d + e - 1))
+
+
+
+# year = int(input("Введите год: "))
+# if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+#     print("Yes")
+# else:
+#     print("No")
