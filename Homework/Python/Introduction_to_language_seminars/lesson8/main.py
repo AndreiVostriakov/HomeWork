ac073e2671873674ca21e99c557fcaac94b2bad5
Andrei_Vostriakov <cursed25@yandex.ru>
2023-07-12 00:00:34 +0500

    Добавил решение преподавателя

--- Homework/Python/Introduction_to_language_seminars/lesson8/main.py
+++ Homework/Python/Introduction_to_language_seminars/lesson8/main.py
@@ -106,3 +106,32 @@ if __name__ == '__main__':
 
 # Код преподавателя
 
+# def print_info(temp):
+#     print(temp)
+
+
+# def read_file():
+#     with open('my_file.txt', 'r', encoding='utf-8') as file:
+#         temp = file.readlines()
+#     return temp
+
+# def write_file():
+#     pass
+
+# def menu():
+#     data = read_file()
+#     while True:
+#         print('Выберите действие: ')
+#         print('1 - вывести инфо на экран')
+#         print('0 - выход из программы')
+#         n = int(input('ваш выбор: '))
+#         if n == 0:
+#             break
+#         elif n == 1:
+#             print_info(data)
+#         # elif n == 3:
+#         #     data = append(data)
+
+
+# if __name__ == '__main__':
+#     menu()
