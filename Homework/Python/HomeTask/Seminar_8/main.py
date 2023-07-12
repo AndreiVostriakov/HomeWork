6afaccdef329d456feff2e0727a116fd38ab8021
WorkStation <Karatel28@mail.ru>
2023-07-12 20:39:21 +0500

    Начало проекта

--- Homework/Python/HomeTask/Seminar_8/main.py
+++ Homework/Python/HomeTask/Seminar_8/main.py
@@ -1,13 +1,13 @@
 
 def read_file():
-    with open('my_file.txt', 'r', encoding='utf-8') as f:
+    with open('phone_book.txt', 'r', encoding='utf-8') as f:
         for string in f:
             print(string)
 
 def write_file():
     surname, name, phone = input("Введите Фамилию, Имя и телефон: ").split()
-    with open('my_file.txt', 'a', encoding='utf-8') as f:
-        f.writelines(f'\n{surname};')
+    with open('phone_book.txt', 'a', encoding='utf-8') as f:
+        f.writelines(f'{surname};')
         f.writelines(f'{name};')
         f.writelines(f'{phone}\n')
 
@@ -15,11 +15,13 @@ def write_file():
 def serch_for_entry():
     pass
 
+
+
 def menu():
+    data = read_file()
     flag = True
     while flag:
         n = int(input('1 - поиск из справочника\n2 - запись в справочник\n3 - поиск\n0 - выход\n'))
-        
         if n == 1:
             read_file()
         elif n == 2:
@@ -33,3 +35,54 @@ def menu():
 
 if __name__ == '__main__':
     menu()
+    
+    
+    
+    
+def read_file():
+    with open('phone_book.txt', 'r', encoding='utf-8') as f:
+        for string in f:
+            print(string)
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+# def print_info(temp):
+#     print(temp)
+
+
+# def read_file():
+#     with open('my_file.txt', 'r', encoding='utf-8') as file:
+#         temp = file.readlines()
+#     return temp
+
+# def write_file():
+#     pass
+
+# def menu():
+#     data = read_file()
+#     while True:
+#         print('Выберите действие: ')
+#         print('1 - вывести инфо на экран')
+#         print('0 - выход из программы')
+#         n = int(input('ваш выбор: '))
+#         if n == 0:
+#             break
+#         elif n == 1:
+#             print_info(data)
+#         # elif n == 3:
+#         #     data = append(data)
+
+
+# if __name__ == '__main__':
+#     menu()
